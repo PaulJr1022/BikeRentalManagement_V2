a3c42735f44cb0deea4c9888c3955a2de3eeadf6
Jesuratnam P. <UT01185@unicomtic.com>
2024-10-11 12:34:26 +0530

    Q1

---
+++ BikeRentalManagement_V2/Bikes.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeRentalManagement_V2
+{
+    internal class Bikes
+    {
+
+        private string BikeId { get; set; }
+        private string Brand { get; set; }
+        private string Model { get; set; }
+        private decimal RentalPrice { get; set; }
+    }
+}
